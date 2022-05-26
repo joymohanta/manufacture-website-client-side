75fb84859cede6c3d80c4f8a91a84b2323e8fd7c
joymohanta <joymohanta06@gmail.com>
2022-05-26 23:02:57 +0600

    finally done

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ManageAllOrders = () => {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    fetch("https://secure-bayou-16364.herokuapp.com/order")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
   return (
     <div>
-      <h2>admin page here all orders will show</h2>
+      <h2>admin page here all orders will show {orders.length} </h2>
     </div>
   );
 };
